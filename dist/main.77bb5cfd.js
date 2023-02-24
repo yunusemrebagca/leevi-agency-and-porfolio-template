13d658e6d40359884ab4430e3010b85232804af3
yunusemrebagca <a7yunusemre34@gmail.com>
2023-02-24 21:05:30 +0300

    added featured section and made minor improvements

--- dist/main.77bb5cfd.js
+++ dist/main.77bb5cfd.js
@@ -172,7 +172,7 @@ module.exports = reloadCSS;
 var reloadCSS = require('_css_loader');
 module.hot.dispose(reloadCSS);
 module.hot.accept(reloadCSS);
-},{"_css_loader":"../node_modules/parcel-bundler/src/builtins/css-loader.js"}],"../node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+},{"C:\\Users\\Q7_yu\\Desktop\\leevi agency and porfolio\\src\\multimedia\\featured_img.png":[["featured_img.d7ca389c.png","multimedia/featured_img.png"],"multimedia/featured_img.png"],"C:\\Users\\Q7_yu\\Desktop\\leevi agency and porfolio\\src\\multimedia\\featured_img-2.png":[["featured_img-2.0d1715cc.png","multimedia/featured_img-2.png"],"multimedia/featured_img-2.png"],"C:\\Users\\Q7_yu\\Desktop\\leevi agency and porfolio\\src\\multimedia\\featured_img-3.png":[["featured_img-3.2cb1b50f.png","multimedia/featured_img-3.png"],"multimedia/featured_img-3.png"],"_css_loader":"../node_modules/parcel-bundler/src/builtins/css-loader.js"}],"../node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
@@ -197,7 +197,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "49914" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "54215" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
